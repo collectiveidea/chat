11f73c994425a063f70789dbeca3b0b4fd13777b
Steve Richert <steve.richert@gmail.com>
2011-08-03 13:00:03 -0400

    Add user tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   has_many :messages
 
   attr_accessible
+
+  before_create :set_token
+
+  private
+    def set_token
+      self.token = ActiveSupport::SecureRandom.urlsafe_base64
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110803161858) do
+ActiveRecord::Schema.define(:version => 20110803165723) do
 
   create_table "messages", :force => true do |t|
     t.integer  "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20110803161858) do
     t.string   "avatar_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "token"
   end
 
 end
