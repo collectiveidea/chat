c3b8f1168079e3f666eae6e64edbfb2f0d7c8306
Steve Richert <steve.richert@gmail.com>
2011-08-03 11:41:40 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = '5e829dce378adc64db250e36255fddd8a57c92b6b96bd2eddb4a60799328054485dd5d80f3b5a42d43e8ab7d3f079c6e699fb9f73f12e32e580bfd16ac442bb9'
