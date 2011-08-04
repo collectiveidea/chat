5a2a2dfac6ef576f69501bb2d30e27fb55a459da
Steve Richert <steve.richert@gmail.com>
2011-08-04 11:02:47 -0400

    Make the app secret configurable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Chat::Application.config.secret_token = '5e829dce378adc64db250e36255fddd8a57c92b6b96bd2eddb4a60799328054485dd5d80f3b5a42d43e8ab7d3f079c6e699fb9f73f12e32e580bfd16ac442bb9'
+Chat::Application.config.secret_token = ENV["SECRET"]
