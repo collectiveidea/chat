ce8cf98846affe724c5071ae3412fc9c6ab9b7d0
Steve Richert <steve.richert@gmail.com>
2011-08-04 14:09:55 -0400

    Clean up message body output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,19 @@ module ApplicationHelper
     image_tag user.avatar_url, :class => "avatar", :alt => user.name, :title => user.name
   end
 
+  def clean_body(body)
+    simple_format auto_link(once_escaped(body)){|l| short_link(l) }
+  end
+
+  def once_escaped(text)
+    begin
+      old_text = text
+      text = CGI.unescape_html(old_text)
+    end until text == old_text
+    h(text)
+  end
+
   def short_link(link)
-    truncate link.sub(/^https?:\/\//i, ""), :omission => "…"
+    truncate link.sub(/^https?:\/\//i, "").sub(/\/$/, ""), :omission => "…"
   end
 end
