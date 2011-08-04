cc88cdd5b2365d7e64cb8ac4d58072d1fef8eb68
Steve Richert <steve.richert@gmail.com>
2011-08-04 13:49:40 -0400

    Truncate links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
+# encoding: utf-8
+
 module ApplicationHelper
   def avatar(user)
     image_tag user.avatar_url, :class => "avatar", :alt => user.name, :title => user.name
+  end
+
+  def short_link(link)
+    truncate link.sub(/^https?:\/\//i, ""), :omission => "…"
   end
 end
