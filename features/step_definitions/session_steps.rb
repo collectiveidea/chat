44ad655b2a9d69ebab9278ec1291b61bef77209f
Steve Richert <steve.richert@gmail.com>
2011-08-04 12:19:29 -0400

    Update the über negation regular expression

--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -2,7 +2,7 @@ When /^I am in (.*) browser$/ do |name|
   Capybara.session_name = name
 end
 
-When /^(?!I am)(.*) in (.*) browser$/ do |step, name|
+When /^(?!I am in)(.*(?= in)) in (.*) browser$/ do |step, name|
   When %(I am in #{name} browser)
   And step
 end
